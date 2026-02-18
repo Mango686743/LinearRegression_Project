8c8081f527454e77bc973a0de2cd7379cb788a6d
hawkie <hawkie@DESKTOP-3CBJIMA.localdomain>
2026-02-18 10:04:03 +0700

    Build khung project

---
+++ linear_regression.py
@@ -0,0 +1,14 @@
+class LinearRegression:         #Predict y = ax+b
+    def __init__(self, learning_rate = 0.01, n_iters = 1000 ):
+        self.lr = learning_rate
+        self.n_iters = n_iters
+        self.weight = None
+        self.bias = None
+        
+    def fit(self,x,y):
+        #Training find a,b
+        pass
+    
+    def predict(self, x):
+        #predict
+        pass
