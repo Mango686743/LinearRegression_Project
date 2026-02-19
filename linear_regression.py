e7004daa44b07d4a21e9e95cd9465355465ff103
Hawkie <dinhdinh16112007@gmail.com>
2026-02-19 21:35:00 +0700

    Code lai + Sua file main bug matrix dong 8

--- linear_regression.py
+++ linear_regression.py
@@ -1,32 +1,27 @@
 import numpy as np
 
-class LinearRegression:         #Predict y = wx+b
-    def __init__(self, learning_rate = 0.01, n_iters = 1000 ):
+class LinearRegression:
+    
+    def __init__(self,learning_rate=0.01, n_iters=1000):
         self.lr = learning_rate
         self.n_iters = n_iters
-        self.weight = None      #Trọng số: a
-        self.bias = None        #Độ lệch: b
+        self.weight = 0
+        self.bias = 0
+    
+    def fit(self,X,y):
+        m_rows, n_cols = X.shape
         
-    def fit(self,X,y):                  #Ma trận X và y
-        #Training find w,b
-        m_rows, n_columns = X.shape     #Ma trận X có m hàng n cột        
-        
-        #Khởi tạo 0
-        self.weight = np.zeros(n_columns)           #Tạo 1 cột vecto 0    
+        self.weight = np.zeros(n_cols)     
         self.bias = 0
         
-        #Học 1000 lần
         for _ in range(self.n_iters):
             y_pred = np.dot(X,self.weight) + self.bias
             
-            #Gradient descent(Đạo hàm) = Hàm Loss
-            dw = (1/m_rows) * np.dot(X.T,y_pred-y)
-            db = (1/m_rows) * np.sum(y_pred-y)       
+            dw = (1/m_rows)*np.dot(X.T,(y_pred-y))
+            db = (1/m_rows)*np.sum(y_pred-y)
             
-            #Update w và b -> fit
             self.weight = self.weight - self.lr*dw
             self.bias = self.bias - self.lr*db
-    
-    def predict(self, X):
-        #predict
-        return np.dot(X, self.weight) + self.bias
+            
+    def predict(self,X):
+        return np.dot(X,self.weight)+self.bias
--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import numpy as np
 import pandas as pd
 from linear_regression import LinearRegression
 
-#Đọc từ file csv bằng pandas
 df = pd.read_csv("data.csv")
 
-#Chuyển dữ liệu dataframe pandas -> arr numpy cho dễ tính toán
-X = df[['study_hours']].values
-y = df[['score']].values
+X_ = df[["study_hours"]].values
+y_ = df["score"].values
 
 model = LinearRegression(learning_rate=0.01, n_iters=1000)
-model.fit(X,y)
+model.fit(X_,y_)
 
-X_test = np.array([[15]])
-print(model.predict(X_test)[0][0])
+testcase = np.array([[15]])
+res = model.predict(testcase)
+
+print(res[0])
