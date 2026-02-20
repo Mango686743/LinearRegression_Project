cc4155e2908e6d3320b1ceb66327278f49471379
Hawkie <dinhdinh16112007@gmail.com>
2026-02-20 20:58:38 +0700

    Update + Fix bug limit points by basix fuction(not recommend)

--- linear_regression.py
+++ linear_regression.py
@@ -1,27 +1,32 @@
 import numpy as np
 
-class LinearRegression:
-    
-    def __init__(self,learning_rate=0.01, n_iters=1000):
+class LinearRegression:         #y = w(n).x^(n) + w(n-1).x^(n-1) + ... + w.x + b
+    def __init__(self, learning_rate = 0.01, n_iters=1000):
         self.lr = learning_rate
         self.n_iters = n_iters
-        self.weight = 0
-        self.bias = 0
-    
-    def fit(self,X,y):
+        self.weight = 0         #matrix of w
+        self.bias = 0           #b
+        
+    def fit(self,X,y):          #Matrix of weight X; Matrix of result y
+        #Matrix size
         m_rows, n_cols = X.shape
         
-        self.weight = np.zeros(n_cols)     
-        self.bias = 0
+        #Initialization
+        self.weight = np.zeros(n_cols)      #Vector 0 size nx1
+        self.bias = 0                       #Auto transform to vecto -> Calculating
         
+        #Calculating
         for _ in range(self.n_iters):
-            y_pred = np.dot(X,self.weight) + self.bias
+            #Proj of y -> Find Least Square
+            y_proj = np.dot(X,self.weight) + self.bias      
             
-            dw = (1/m_rows)*np.dot(X.T,(y_pred-y))
-            db = (1/m_rows)*np.sum(y_pred-y)
-            
-            self.weight = self.weight - self.lr*dw
-            self.bias = self.bias - self.lr*db
+            #Gradient descent by Loss Fuction
+            dw = (1/m_rows)*np.dot(X.T,(y_proj-y))
+            db = (1/m_rows)*np.sum(y_proj-y)
+        
+            #Machine learning for finding fitter W and B
+            self.weight -= self.lr*dw
+            self.bias   -= self.lr*db
             
     def predict(self,X):
-        return np.dot(X,self.weight)+self.bias
+        return np.dot(X,self.weight) + self.bias        
--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from linear_regression import LinearRegression
 
 df = pd.read_csv("data.csv")
 
-X_ = df[["study_hours"]].values
-y_ = df["score"].values
+X_test = df[["study_hours","gaming_hours","sleeping_hours"]].values     #List of dictionary -> Matrix of Weight
+y_test = df["score"].values                                             #A vector of result y
 
-model = LinearRegression(learning_rate=0.01, n_iters=1000)
-model.fit(X_,y_)
+model = LinearRegression(learning_rate=0.001,n_iters=1000)
+model.fit(X_test,y_test)
 
-testcase = np.array([[15]])
-res = model.predict(testcase)
+A_crazy_student = np.array([[13,0.5,8]])           
+res = model.predict(A_crazy_student)
 
-print(res[0])
+#New bug: Maximum and Minimum of points is [0,100] -> Limits the res for pratical situation
+res = np.clip(res,0,100)        #Not recommend
+
+##Future Work Here
+
+print(f"According to the reseacher, it's vividly that the student, who studies {A_crazy_student[0][0]} hours, plays game {A_crazy_student[0][1]} hours and sleeps {A_crazy_student[0][2]} hours, will gain {res[0]} out of 100")
